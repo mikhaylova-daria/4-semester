0ff7fe96b63dd64d9c3eddba560ccd9e9f027974
mikhaylova-daria <dasham94@yandex.ru>
2014-02-15 01:45:20 +0400

    condition_variable

--- pool/main.cpp
+++ pool/main.cpp
@@ -16,9 +16,10 @@ int check_array[number];
 struct functor1 {
     void operator()(int i) {
         check_array[i] = i;
-       // std::cout<<"Hello, world!"<<i<<std::endl;
+        //std::cout<<"Hello, world!"<<i<<std::endl;
     }
 };
+
 
 
 template <typename return_type, typename ... arg_types>
@@ -27,43 +28,54 @@ class thread_pool  {
     class executant_thread {
         int id;
         thread_pool* pool_ptr;
+        struct finish_flag {
+
+        };
+
     public:
         executant_thread(thread_pool* _pool_ptr, int _id): pool_ptr(_pool_ptr), id(_id) {}
         ~executant_thread() {}
         void listen() {
-            if (!pool_ptr->tasks.empty()){
-                pool_ptr->lock_for_queuecheck.lock();
-                if (pool_ptr->tasks.empty()) {
-                    pool_ptr->lock_for_queuecheck.unlock();
-                } else {
-                    std::pair<std::function <return_type(arg_types...)>, arg_types... > funct;
-                    funct = pool_ptr->tasks.back();
-                    pool_ptr->tasks.pop_back();
-                    funct.first(funct.second);
-                    pool_ptr->lock_for_queuecheck.unlock();
-                }
+            std::unique_lock<std::mutex> locker(pool_ptr->lock_for_queuecheck);
+            pool_ptr->queuecheck.wait(locker);
+            if (!pool_ptr->tasks.empty()) {
+                std::pair<std::function <return_type(arg_types...)>, arg_types... > funct = pool_ptr->tasks.back();
+                pool_ptr->tasks.pop_back();
+                locker.unlock();
+                funct.first(funct.second);
             }
+
         }
     };
 
     struct start_executent_thread {
         void operator()(thread_pool* pool_ptr, int id) {
             thread_pool::executant_thread executant(pool_ptr, id);
-            while (!pool_ptr->finish) {
-                executant.listen();
-            }
-            while (!pool_ptr->tasks.empty()) {
+            while (true) {
                 executant.listen();
             }
         }
     };
 
 
+//    class concurrent_vector {
+//        std::vector<std::pair<std::function<return_type(arg_types ...) >, arg_types...  > > vect;
+//    public:
+//        concurrent_vector() {}
+//        ~concurrent_vector() {}
+//        std::pair<std::function <return_type(arg_types...)>, arg_types... > top() {
+//            std::pair<std::function <return_type(arg_types...)>, arg_types... > answer = vect.front()
+//            return
+//        }
+
+//    };
+
 public:
-    std::atomic<bool> finish;
+    bool finish;
     std::vector<boost::thread> executant_threads;
     std::vector<std::pair<std::function<return_type(arg_types ...) >, arg_types...  > > tasks;
     std::mutex lock_for_queuecheck;
+    std::condition_variable queuecheck;
 public :
     thread_pool() {
         finish = false;
@@ -73,19 +85,21 @@ public :
         }
     }
     ~thread_pool() {
-        while (!tasks.empty()) {}
-        finish = true;
+        while (!tasks.empty()) {
+            queuecheck.notify_one();
+        }
         for (int i = 0; i < executant_threads.size(); ++i) {
-            executant_threads[i].join();
+            executant_threads[i].interrupt();
             std::cout<<"нить завершилась"<<std::endl;
         }
+
     }
 
     void execute(std::function<return_type(arg_types...)> func, arg_types ... args) {
         lock_for_queuecheck.lock();
         tasks.push_back(std::make_pair(func, args...));
-   //     std::cout<<"!!"<<std::endl;
         lock_for_queuecheck.unlock();
+        queuecheck.notify_one();
     }
 };
 
@@ -100,7 +114,7 @@ int main()
     }
     for (int i = 1; i < number; ++i) {
         if (check_array[i] != i) {
-            std::cout<<"fail";
+            std::cout<<"fail"<<i<<std::endl;
         }
     }
     return 0;
